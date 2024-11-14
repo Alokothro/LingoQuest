0e6deb80dbf8dc5242c89a0bce590403af926242
Preston <prwillis2@gmail.com>
2024-11-14 14:22:30 -0500

    still 3 errors in module-info

--- lingoquest2/src/main/java/module-info.java
+++ lingoquest2/src/main/java/module-info.java
@@ -1,6 +1,17 @@
 module lingoquestpackage {
     requires javafx.controls;
     requires javafx.fxml;
+    requires junit;
+    requires json.simple;
+    requires jlayer;
+    requires software.amazon.awssdk.core;
+    requires software.amazon.awssdk.services.polly;
+    requires software.amazon.awssdk.regions;
+    requires software.amazon.awssdk.utils;
+    requires org.slf4j;
+    requires org.slf4j.simple;
+    requires software.amazon.awssdk.awscore;
+    requires software.amazon.eventstream;
 
     opens lingoquestpackage to javafx.fxml;
     exports lingoquestpackage;
