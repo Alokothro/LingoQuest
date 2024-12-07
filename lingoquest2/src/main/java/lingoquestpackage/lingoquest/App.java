afddbc594704d516ba15305d2b6bc6aa7d4456d7
Alok Patel <alokothro@gmail.com>
2024-12-07 18:17:44 -0500

    Documented App.java

--- lingoquest2/src/main/java/lingoquestpackage/lingoquest/App.java
+++ lingoquest2/src/main/java/lingoquestpackage/lingoquest/App.java
@@ -15,6 +15,13 @@ public class App extends Application {
 
     private static Scene scene;
 
+    /**
+     * The entry point for the JavaFX application.
+     * This method is called when the JavaFX application is launched.
+     *
+     * @param stage The primary stage (window) for the JavaFX application.
+     * @throws Exception if the FXML file fails to load.
+     */
     @Override
     public void start(Stage stage) throws Exception {
         //LanguageGame.getInstance().login("TTomacka", "Password1234");
@@ -24,15 +31,35 @@ public class App extends Application {
         stage.show();
     }
 
+    /**
+     * Sets the root node of the scene to the specified FXML file.
+     * This allows switching between different screens in the application.
+     *
+     * @param fxml The path to the FXML file to load as the new root.
+     * @throws IOException if the FXML file cannot be loaded.
+     */
     public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
-
+    
+    /**
+     * Helper method to load an FXML file and return the root node.
+     *
+     * @param fxml The path to the FXML file to load (without the .fxml extension).
+     * @return The root node loaded from the FXML file.
+     * @throws IOException if the FXML file cannot be loaded.
+     */
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
-
+    
+    /**
+     * The main entry point for the application.
+     * This method launches the JavaFX application.
+     *
+     * @param args Command-line arguments passed to the application.
+     */
     public static void main(String[] args) {
         launch();
     }
