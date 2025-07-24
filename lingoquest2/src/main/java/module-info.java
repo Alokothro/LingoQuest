1697daf2bb5c92630b5090ccd2099f5bfd2f0c0b
Alok Patel <alokothro@gmail.com>
2025-07-23 19:26:26 -0500

    Revert "Fix: Maven configuration and JavaFX module system for Apple Silicon"
    This reverts commit cd66bd8a304d2aea97b8167f8288b1fb3e66f8b8.

--- lingoquest2/src/main/java/module-info.java
+++ lingoquest2/src/main/java/module-info.java
@@ -1,25 +1,31 @@
 module lingoquestpackage {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.base;
+    requires junit;
     requires json.simple;
     requires jlayer;
     requires software.amazon.awssdk.core;
     requires software.amazon.awssdk.services.polly;
     requires software.amazon.awssdk.regions;
     requires software.amazon.awssdk.utils;
-    requires software.amazon.awssdk.awscore;
-    requires software.amazon.eventstream;
     requires org.slf4j;
     requires org.slf4j.simple;
-    
+    requires software.amazon.awssdk.awscore;
+    requires software.amazon.eventstream;
+    requires javafx.base;
+
     opens lingoquestpackage.controllers to javafx.fxml;
-    opens lingoquestpackage.lingoquest to javafx.fxml;
-    opens lingoquestpackage.models to javafx.fxml;
-    opens lingoquestpackage.narriator to javafx.fxml;
-    
     exports lingoquestpackage.controllers;
+
+    opens lingoquestpackage.lingoquest to javafx.fxml;
     exports lingoquestpackage.lingoquest;
+
+    opens lingoquestpackage.models to javafx.fxml;
     exports lingoquestpackage.models;
+
+    opens lingoquestpackage.narriator to javafx.fxml;
     exports lingoquestpackage.narriator;
+
+    //opens lingoquestpackage.data to javafx.fxml;
+    //exports lingoquestpackage.data;
 }
